bcb2bca258febc5a3ca9a00fbe2bec70e902840a
Ben Zellers <zellersdenver@gmail.com>
2023-05-15 15:27:53 -0400

    Exercise 2 completed

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,31 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            
+            string subject = Console.ReadLine();
+            
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is awesome!");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym is fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("I love History.");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is the best!");
+                    break;
+                case "english":
+                    Console.WriteLine("English can get really boring.");
+                    break;
+                default:
+                    Console.WriteLine("That is not a subject that I recognize!");
+                    break;
+            }
         }
     }
 }
